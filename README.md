545ab32dfd5058387b368641be4d8185ce168fb8
KEvans254 <evanstoja@gmail.com>
2023-03-23 19:11:59 +0300

    Relevant files

---
+++ README.md
@@ -0,0 +1,33 @@
+# Recipe Search
+This is a web application that allows users to search for recipes based on various criteria such as ingredients, dietary restrictions, and cuisine type. The application uses the Edamam API to fetch recipe data and display it in a user-friendly format.
+
+## Features
+- Search for recipes by entering keywords and filters
+- View recipes with details such as ingredients, cooking instructions, and nutritional information
+- Pagination to navigate through multiple pages of search results
+
+## Technologies Used
+- HTML5
+- CSS3
+- JavaScript
+- Edamam API
+
+## Getting Started
+To get started with the project, follow these steps:
+
+1. Clone the repository to your local machine:
+
+`git clone https://github.com/your-username/recipe-search.git`
+
+1. Open the index.html file in your web browser to view the application.
+
+## Usage
+To use the application, follow these steps:
+
+1. Enter a search query and select any desired filters (e.g. include ingredients, exclude ingredients, diet, cuisine type).
+1. Click the "Search" button to fetch recipe results from the Edamam API.
+1. View the search results and click on any recipe to view its details.
+1. Use the pagination buttons to navigate through multiple pages of search results.
+
+## License
+This project is licensed under the MIT License. 
---
+++ script.js
@@ -0,0 +1,68 @@
+const appId = '7cc1191d';
+const appKey = '408b5ba93e0d384e5fd58fcbcd65f649';
+
+const searchBtn = document.getElementById('search-btn');
+searchBtn.addEventListener('click', searchRecipes);
+
+function searchRecipes() {
+  const query = document.getElementById('query').value;
+  const includeIngredients = document.getElementById('includeIngredients').value;
+  const excludeIngredients = document.getElementById('excludeIngredients').value;
+  const diet = document.getElementById('diet').value;
+  const cuisineType = document.getElementById('cuisineType').value;
+
+  const apiUrl = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}&`;
+
+  if (includeIngredients) {
+    apiUrl += `&ingr=${includeIngredients}`;
+  }
+
+  if (excludeIngredients) {
+    apiUrl += `&excluded=${excludeIngredients}`;
+  }
+
+  if (diet) {
+    apiUrl += `&diet=${diet}`;
+  }
+
+  if (cuisineType) {
+    apiUrl += `&cuisineType=${cuisineType}`;
+  }
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => displayResults(data.hits))
+    .catch(error => displayError(error));
+}
+
+function displayResults(hits) {
+  const results = document.getElementById('results');
+  results.innerHTML = '';
+
+  if (hits.length === 0) {
+    const errorMessage = 'No results found. Please try another search.';
+    displayError(errorMessage);
+  } else {
+    hits.forEach(hit => {
+      const { recipe } = hit;
+      const { label, image, url, ingredients } = recipe;
+
+      const li = document.createElement('li');
+      const img = document.createElement('img');
+      img.src = image;
+
+      const div = document.createElement('div');
+      div.innerHTML = `<h2><a href="${url}" target="_blank">${label}</a></h2><p>${ingredients.length} ingredients</p>`;
+
+      li.appendChild(img);
+      li.appendChild(div);
+
+      results.appendChild(li);
+    });
+  }
+}
+
+function displayError(errorMessage) {
+  const errorDiv = document.getElementById('error-message');
+  errorDiv.innerHTML = errorMessage;
+}
