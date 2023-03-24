00811ce27820c407b5ca4ee8f750d59fbe0e74bd
Evans Kiprotich <79991349+KEvans254@users.noreply.github.com>
2023-03-24 02:45:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,13 @@ const searchBtn = document.getElementById('search-btn');
 searchBtn.addEventListener('click', searchRecipes);
 
 function searchRecipes() {
-  const query = document.getElementById('query').value;
-  const includeIngredients = document.getElementById('includeIngredients').value;
-  const excludeIngredients = document.getElementById('excludeIngredients').value;
-  const diet = document.getElementById('diet').value;
-  const cuisineType = document.getElementById('cuisineType').value;
+  const query = document.getElementById('query').value.trim();
+  const includeIngredients = document.getElementById('includeIngredients').value.trim();
+  const excludeIngredients = document.getElementById('excludeIngredients').value.trim();
+  const diet = document.getElementById('diet').value.trim();
+  const cuisineType = document.getElementById('cuisineType').value.trim();
 
-  // Check if required fields are filled
-  if (!query) {
-    displayError('Please enter a search query');
-    return;
-  }
-
-  const apiUrl = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}`;
+  let apiUrl = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}`;
 
   if (includeIngredients) {
     apiUrl += `&ingr=${includeIngredients}`;
@@ -35,40 +29,60 @@ function searchRecipes() {
     apiUrl += `&cuisineType=${cuisineType}`;
   }
 
+  if (!query) {
+    const errorMessage = 'Please enter a search term.';
+    displayError(errorMessage);
+    return;
+  }
+
   fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => displayResults(data.hits))
-    .catch(error => displayError(error));
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.hits.length === 0) {
+        const errorMessage = 'No results found. Please try another search.';
+        displayError(errorMessage);
+      } else {
+        displayResults(data.hits);
+      }
+    })
+    .catch(error => {
+      const errorMessage = 'An error occurred while fetching the data.';
+      displayError(errorMessage);
+      console.error(error);
+    });
 }
 
 function displayResults(hits) {
   const results = document.getElementById('results');
   results.innerHTML = '';
 
-  if (hits.length === 0) {
-    const errorMessage = 'No results found. Please try another search.';
-    displayError(errorMessage);
-  } else {
-    hits.forEach(hit => {
-      const { recipe } = hit;
-      const { label, image, url, ingredients } = recipe;
+  hits.forEach(hit => {
+    const { recipe } = hit;
+    const { label, image, url, ingredients } = recipe;
 
-      const li = document.createElement('li');
-      const img = document.createElement('img');
-      img.src = image;
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    img.src = image;
 
-      const div = document.createElement('div');
-      div.innerHTML = `<h2><a href="${url}" target="_blank">${label}</a></h2><p>${ingredients?.length || 0} ingredients</p>`;
+    const div = document.createElement('div');
+    div.innerHTML = `<h2><a href="${url}" target="_blank">${label}</a></h2><p>${ingredients.length} ingredients</p>`;
 
-      li.appendChild(img);
-      li.appendChild(div);
+    li.appendChild(img);
+    li.appendChild(div);
 
-      results.appendChild(li);
-    });
-  }
+    results.appendChild(li);
+  });
 }
 
 function displayError(errorMessage) {
+  const results = document.getElementById('results');
+  results.innerHTML = '';
+
   const errorDiv = document.getElementById('error-message');
   errorDiv.innerHTML = errorMessage;
 }
